177d3dfdecd3680f5f9872748b7f131032311bd5
rezwanrahimr <rezwanrahim69@gmail.com>
2022-09-21 23:21:50 +0600

    work simple done

--- src/Pages/Home/Work/WorkSimble.js
+++ src/Pages/Home/Work/WorkSimble.js
@@ -4,8 +4,8 @@ import worlSimble from './workSimble.css';
 
 const WorkSimble = () => {
     return (
-        <div className='workSimble '>
-             <h2 className='text-2xl font-bold mb-12 mt-32 text-white'>Provide awesome <span style={{ color: '#7AB259' }}>services</span></h2>
+        <div className='workSimble mt-28'>
+             <h2 className='text-2xl font-bold mb-12 mt-32 text-white'>Here are some of <span style={{ color: '#7AB259' }}>our works</span></h2>
                <div className='flex items-center '>
                <Work className=''></Work>
                </div>
